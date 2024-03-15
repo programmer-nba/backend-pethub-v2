cebf91be5ab396532fba694cb63d748c538895d3
Programmer NBA <programmer@nbadigitalworlds.com>
2024-03-15 17:19:59 +0700

    Update member.js

--- router/user/member.js
+++ router/user/member.js
@@ -3,7 +3,7 @@ const member = require("../../controllers/user/member.controller");
 const authAdmin = require("../../lib/auth.admin");
 const auth = require("../../lib/auth");
 
-router.post("/create", authAdmin, member.create);
+router.post("/create", auth, member.create);
 router.get("/", authAdmin, member.getMemberAll);
 router.get("/:id", auth, member.getMemberById);
 router.get("/tel/:id", member.getMemberByTel);
