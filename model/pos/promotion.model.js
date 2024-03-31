de21753b891c4e09eec23ca78f7b3ff13c4d0d0a
Programmer NBA <programmer@nbadigitalworlds.com>
2024-03-31 15:03:21 +0700

    Update promotion.model.js

--- model/pos/promotion.model.js
+++ model/pos/promotion.model.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 
 const PromotionSchema = new mongoose.Schema({
     promotion_name: { type: String, required: true },
-    product_barcode: { type: String, required: true },
-    product_price: { type: Number, required: true },
-    product_amount: { type: Number, required: true },
+    promotion_type: { type: String, enum: ['ลดราคาตามจำนวน', 'แถมตามจำนวน'], required: true },
+    product_barcode: { type: Array, required: false, default: [] },
+    product_price: { type: Number, required: false, default: 0 },
+    product_amount: { type: Number, required: false, default: 0 },
     promotion_start: { type: Date, required: true, default: Date.now() },
     promotion_end: { type: Date, required: true, default: Date.now() },
     // promotion_detail: { type: String, required: false, default: "ไม่มี" },
@@ -16,9 +17,10 @@ const Promotions = mongoose.model("promotion", PromotionSchema);
 const validate = (data) => {
     const schema = Joi.object({
         promotion_name: Joi.string().required().label("กรอกชื่อโปรโมชั่น"),
-        product_barcode: Joi.string().required().label("กรอกบาร์โค๊ดสินค้า"),
-        product_price: Joi.number().required().label("กรอกราคา"),
-        product_amount: Joi.number().required().label("กรอกจำนวนสินค้า"),
+        promotion_type: Joi.string().required().label("กรอกประเภทโปรโมชั่น"),
+        product_barcode: Joi.array().default([]),
+        product_price: Joi.number().default(0),
+        product_amount: Joi.number().default(0),
         promotion_start: Joi.date().required().label("กรอกวันที่เริ่ม"),
         promotion_end: Joi.date().required().label("กรอกวันที่สิ้นสุด"),
         // promotion_detail: Joi.string().default("ไม่มี"),
